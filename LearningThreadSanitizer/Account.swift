939907e78642f227bd5d377446921d3d2d29ea0c
Rommel Rico <me@rommelrico.com>
2017-10-23 08:56:31 -0700

    The account model

---
+++ LearningThreadSanitizer/Account.swift
@@ -0,0 +1,36 @@
+//
+//  Account.swift
+//  LearningThreadSanitizer
+//
+//  Created by Rommel Rico on 10/23/17.
+//  Copyright © 2017 Rommel Rico. All rights reserved.
+//
+
+import Foundation
+
+class Account {
+    var balance: Int = 0
+    
+    func withdraw(amount: Int, completed: () -> ()) {
+        let newBalance = self.balance - amount
+        
+        if newBalance < 0 {
+            print("You don't have enough money to withdraw \(amount)")
+            return
+        }
+        
+        // Simulate processing of fraud checks
+        sleep(2)
+        
+        self.balance = newBalance
+        
+        completed()
+    }
+    
+    func deposit(amount: Int, completed: () -> ()) {
+        let newBalance = self.balance + amount
+        self.balance = newBalance
+        
+        completed()
+    }
+}
