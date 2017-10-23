5d3a1c59030aeec2cd61de0fb5a7e8663d1cc29b
Rommel Rico <me@rommelrico.com>
2017-10-23 09:09:52 -0700

    Moving the withdraw method off the main thread

--- LearningThreadSanitizer/Account.swift
+++ LearningThreadSanitizer/Account.swift
@@ -11,20 +11,24 @@ import Foundation
 class Account {
     var balance: Int = 0
     
-    func withdraw(amount: Int, completed: () -> ()) {
-        let newBalance = self.balance - amount
-        
-        if newBalance < 0 {
-            print("You don't have enough money to withdraw \(amount)")
-            return
+    func withdraw(amount: Int, completed: @escaping () -> ()) {
+        DispatchQueue(label: "com.rommelrico.balance-moderator").async {
+            let newBalance = self.balance - amount
+            
+            if newBalance < 0 {
+                print("You don't have enough money to withdraw \(amount)")
+                return
+            }
+            
+            // Simulate processing of fraud checks
+            sleep(2)
+            
+            self.balance = newBalance
+            
+            DispatchQueue.main.async {
+                completed()
+            }
         }
-        
-        // Simulate processing of fraud checks
-        sleep(2)
-        
-        self.balance = newBalance
-        
-        completed()
     }
     
     func deposit(amount: Int, completed: () -> ()) {
@@ -33,4 +37,5 @@ class Account {
         
         completed()
     }
+    
 }
